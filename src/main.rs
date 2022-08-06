8168cd9aaef1787f277640e00e673c03a2615492
Payton <pjtrosclair@ksu.edu>
2022-08-06 00:15:45 -0500

    More additions to the code

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ const SCREEN_SIZE: (f32, f32) = (
     GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
 );
 
+#[derive(Copy, Clone)]
 enum PieceColor {
     Red,
     Yellow,
@@ -37,11 +38,11 @@ struct GameState {
     rowsClearedCount: i16,
     score: i32,
     hasHeldAPiece: bool,
-    board: Vec<Vec<i32>>
+    board: [[Option<PieceColor>; 10]; 20]
 }
 
 impl GameState {
-    pub fn new() -> Self {
+    pub fn new(board: [[Option<PieceColor>; 10]; 20]) -> Self {
         GameState {
             rowsClearedCount: 0,
             score: 0,
@@ -49,34 +50,127 @@ impl GameState {
             currentPiece: Piece::GetPiece(),
             nextPiece: Piece::GetPiece(),
             holdPiece: None,
-            board: vec![vec![]]
+            board
         }
     }
 
-    pub fn CheckCollision(rotation: [u32; 4], x: u8, y: u8) {
-        GameState::applyFunctionToEachBlockInAPiece(rotation, x, y, (x, y) ,16)
+    pub fn moveDirection(&mut self, direction: GameInput) -> bool {
+        let mut x: usize = self.currentPiece.X;
+        let mut y: usize = self.currentPiece.Y;
+
+        match direction {
+            GameInput::Left => x = x - 1,
+            GameInput::Right => x = x + 1,
+            GameInput::Down => y = y - 1,
+            _ => ()
+        }
+
+        if !GameState::CheckCollision(self, x, y) {
+            self.currentPiece.X = x;
+            self.currentPiece.Y = y;
+            return true;
+        }
+        return false;
     }
 
-    fn IsCollision(x: u8, y: u8) {
-        if (x < 0) || (x >= 10) || (y < 0) || (y >= 20)
+    pub fn drop(&mut self, isHoldingDown: bool) {
+        if isHoldingDown {
+            self.score = self.score + 10
+        }
+
+        if !self.moveDirection(GameInput::Down) {
+            self.after_drop();
+        }
     }
 
-    pub fn applyFunctionToEachBlockInAPiece(rotation: [u32; 4], x: u8, y: u8, function:  fn(u8, u8), iterations: u8) {
-        if iterations > 0
+    pub fn hard_drop(&mut self) -> bool {
+        while self.moveDirection(GameInput::Down) {
+            self.score = self.score + 10
+        }
+        self.score = self.score + 10;
+        self.after_drop();
+
+        return true;
+    }
+
+    pub fn after_drop(&mut self) {
+        self.dropPiece();
+    }
+
+    pub fn hold(&mut self) -> bool {
+
+        return true;
+    }
+
+    pub fn dropPiece(&mut self) {
+        let mut iterations = 0;
+        let mut func = |x: usize, y: usize| {
+            self.board[x][y] = Option::from(self.currentPiece.PieceColor);
+        };
+        while iterations < 16
+        {
+            if (self.currentPiece.getRotationState() & (0x8000 >> iterations)) > 0 {
+                func(self.currentPiece.X + (iterations % 4), self.currentPiece.Y + (iterations / 4));
+            }
+            iterations = iterations + 1;
+        }
+    }
+
+    pub fn rotate(&mut self, direction: GameInput) -> bool {
+        let newRotationState = match direction {
+            GameInput::RotateLeft => (self.currentPiece.RotationState + 3) % 4,
+            GameInput::RotateRight => (self.currentPiece.RotationState + 1) % 4,
+            _ => self.currentPiece.RotationState
+        };
+
+        if !GameState::CheckCollision(self, self.currentPiece.X, self.currentPiece.Y) {
+            self.currentPiece.RotationState = newRotationState;
+        }
+        return true;
+    }
+
+    pub fn getDropShadowY(&mut self) -> usize {
+        let mut y: usize = self.currentPiece.Y;
+        while !GameState::CheckCollision(self, self.currentPiece.X, y) {
+            y = y + 1;
+        }
+        return y - 1;
+    }
+
+    pub fn CheckCollision(&self, x: usize, y: usize) -> bool {
+        let mut b: bool = false;
+        let mut iterations = 0;
+        let mut func = |x: usize, y: usize| {
+            b = b | ((x < 0) || (x >= 10) || (y < 0) || (y >= 20) || (!self.board[x][y].is_none()));
+        };
+        while iterations < 16
+        {
+            if (self.currentPiece.Rotation[self.currentPiece.RotationState] & (0x8000 >> iterations)) > 0 {
+                func(x + (iterations % 4), y + (iterations / 4));
+            }
+            iterations = iterations + 1;
+        }
+        return b;
+    }
+
+    pub fn applyFunctionToEachBlockInAPiece(rotation: u32, x: usize, y: usize, function: fn(usize, usize)) {
+        let mut iterations = 0;
+        while iterations < 16
         {
             if (rotation & (0x8000 >> iterations)) > 0 {
                 function(x + (iterations % 4), y + (iterations / 4));
             }
-            GameState::applyFunctionToEachBlockInAPiece(rotation, x, y, function,iterations - 1);
+            iterations = iterations + 1;
         }
     }
 }
 
+#[derive(Copy, Clone)]
 struct Piece {
     PieceSize: u8,
     RotationState: usize,
-    X: u8,
-    Y: u8,
+    X: usize,
+    Y: usize,
     Rotation: [u32; 4],
     PieceColor: PieceColor
 }
@@ -142,6 +236,8 @@ impl GameInput {
     }
 }
 
+
+
 // Then we implement the `ggez:event::EventHandler` trait on it, which
 // requires callbacks for updating and drawing the game state each frame.
 //
@@ -149,6 +245,8 @@ impl GameInput {
 // that you can override if you wish, but the defaults are fine.
 impl event::EventHandler<ggez::GameError> for GameState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+
+
         Ok(())
     }
 
@@ -163,7 +261,16 @@ impl event::EventHandler<ggez::GameError> for GameState {
 
     fn key_down_event(&mut self, _ctx: &mut Context, input: KeyInput, _repeat: bool) -> GameResult {
         if let Some(dir) = input.keycode.and_then(GameInput::from_keycode){
-
+            let is_successful_move = match dir {
+                GameInput::Down => self.moveDirection(dir),
+                GameInput::Left => self.moveDirection(dir),
+                GameInput::Right => self.moveDirection(dir),
+                GameInput::RotateRight => self.rotate(dir),
+                GameInput::RotateLeft => self.rotate(dir),
+                GameInput::HardDrop => self.hard_drop(),
+                GameInput::Hold => self.hold(),
+                _ => false
+            };
         }
         Ok(())
     }
@@ -182,14 +289,14 @@ pub fn main() -> GameResult {
     // so that ggez will look in our cargo project directory for files.
     let (mut ctx, events_loop) = ggez::ContextBuilder::new("Tetris", "Payton Trosclair")
         // Next we set up the window. This title will be displayed in the title bar of the window.
-        .window_setup(ggez::conf::WindowSetup::default().title("Snake!"))
+        .window_setup(ggez::conf::WindowSetup::default().title("Tetris!"))
         // Now we get to set the size of the window, which we use our SCREEN_SIZE constant from earlier to help with
         .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
         // And finally we attempt to build the context and create the window. If it fails, we panic with the message
         // "Failed to build ggez context"
         .build()?;
 
-
-    let state = GameState::new();
+    let mut board: [[Option<PieceColor>; 10]; 20] = [[None; 10]; 20];
+    let state = GameState::new(board);
     event::run(ctx, events_loop, state)
 }
